4a3beec6766a30dae09b5d3c3e602718aa48e71d
Madhav Gaur <153829578+bt-ech@users.noreply.github.com>
2024-10-29 19:37:42 +0530

    Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,81 +1,28 @@
-/* जय  महाकाल */
+/* ------- Do everything you have to do, but not with ego, not with lust, not with envy but with love, compassion, humility, and devotion.” ― Lord Krishna --- */
 
 #include "bits/stdc++.h"
-using namespace std ;  
+using namespace std ;
 #define endl '\n'
-#define ll long long 
-const long long mx = 1e9 ; 
-const long long Mod = 1e9 + 7 ; 
+const long long MOD = 1e9 + 7 ;
 
-void show2(map<int,int>& mt) //Debug Function 
-{
-	for(auto& i : mt)
-	{
-		cout << "[ ";
-		cout << i.first << ',' << i.second << ' ';
-		cout << "]" << endl ;
-	}
+template <typename T>
+void out(const std::vector<T>& vec) {
+    for (const auto& element : vec) {
+        cout << element << ' ';
+    } cout << endl ;
+    //cout << endl ;
 }
 
-long long getpow(ll a , ll b) //Calculating a ^ b in 0(log(b))
-{
-	long long res = 1;
-	while(b > 0)
-	{
-		if((b & 1)==1)
-		{
-			res *= a ;
-			res %= Mod;
-		}
-		b>>=1;
-		a*=a;
-		a%=Mod;
-	}
-	return res ;
-}
-
-void show1(vector<int>& x)
-{
-	cout << "[ ";
-	for(auto& i : x)
-	{
-		cout << i << ' ';
-	}
-	cout << "]";
-	cout << endl ;
-}
-              // M A I N ___C O D E
-              
-// ALWAYS REMEMBER EASY PROBLEMS WON'T TEACH YOU ANYTHING
-
-// LEARNING ALSO OCCURS THROUGH SEEING OTHER'S SOLUTION 
-
-// EVERY PROBLEM WHETHER EASY OR TOUGH TEACHES YOU SOMETHING -- SO ENJOY LEARNING
-
-void test_case()
-{
-	// take a walk if stuck
-	
+void test_case(){
 	
 }
 
-signed main()
-{
+int main(){
 	ios_base::sync_with_stdio(0);
-	
-	cin.tie(0); cout.tie(0);
-	
-	/* WRITE STUFF DOWN
-    * DON'T GET STUCK ON ONE APPROACH
-    * IF STUCK ON A QUESTION, MOVE TO THE NEXT ONE
-    */
-
-	int t ; cin >> t ; 
-	
-	while(t--)
-	{
+	cin.tie(nullptr);
+	int T ; cin >> T ;
+	//T = 1; 
+	while(T--){
 		test_case();
 	}
-	
-	return 0 ; 
 }
